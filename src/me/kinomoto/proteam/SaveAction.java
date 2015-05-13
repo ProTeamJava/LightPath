91621302bd80e3d60307343a12db60c5d6b8ca17
Monika <monika.goszcz94@gmail.com>
2015-05-13 03:34:53 +0200

    open and save option introductory implementation 2

---
+++ src/me/kinomoto/proteam/SaveAction.java
@@ -0,0 +1,43 @@
+package me.kinomoto.proteam;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
+
+
+public class SaveAction extends AbstractAction {
+	private static final long serialVersionUID = -2516735275557305010L;
+	Main main;
+	public SaveAction(Main ref, JMenuItem name) {
+		super();
+		main = ref;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(main.path == null)
+		{
+			JFileChooser chooser = new JFileChooser();
+			int ret = chooser.showSaveDialog(main);
+			if(ret == JFileChooser.APPROVE_OPTION)
+			{
+				File file = chooser.getSelectedFile();
+				main.path = Paths.get(file.getAbsolutePath());
+			}
+			else
+			{
+				return;
+			}
+		}
+
+		main.surroundingsView.save();
+		main.modyfied = false;
+	}
+
+}
