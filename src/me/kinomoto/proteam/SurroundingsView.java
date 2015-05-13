60b055c1e7a22d5be07da90aba0f4ce9654319eb
Monika <monika.goszcz94@gmail.com>
2015-05-13 03:26:51 +0200

    open and save option introductory implementation

--- src/me/kinomoto/proteam/SurroundingsView.java
+++ src/me/kinomoto/proteam/SurroundingsView.java
@@ -4,19 +4,27 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.RenderedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 
 public class SurroundingsView extends JPanel {
 
 	private static final long serialVersionUID = 5447523639086911950L;
 
+	Main main;
+	
 	Surroundings surroundings = new Surroundings(this);
 	SettingsPanel settingsPanel;
+	JFileChooser fc = new JFileChooser();
 
 	private int baseWidth = 3840;
 	private int baseHeight = 2160;
@@ -26,8 +34,9 @@ public class SurroundingsView extends JPanel {
 	
 	private double scale = 1;
 
-	public SurroundingsView(final SettingsPanel settingsPanel) {
+	public SurroundingsView(final SettingsPanel settingsPanel, Main ref) {
 		this.settingsPanel = settingsPanel;
+		main = ref;
 		
 		this.setPreferredSize(new Dimension(viewWidth, viewHeight));
 
@@ -77,6 +86,7 @@ public class SurroundingsView extends JPanel {
 
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.drawImage(main.open.img, 0, 0, null);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.scale(scale, scale);
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -106,6 +116,28 @@ public class SurroundingsView extends JPanel {
 		if(scale < 0.2) return;
 		scale /= Math.sqrt(2);
 		updateSize();
+	}
+	
+	public void save()
+	{
+
+
+		Image png = createImage(this.getWidth(), this.getHeight()); 
+		Graphics2D paint = (Graphics2D) png.getGraphics();
+		this.paint(paint);
+		int returnVal = fc.showSaveDialog(this);
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			try {
+				if (ImageIO.write((RenderedImage) png, "png", fc.getSelectedFile()))
+				{
+					System.out.println("-- saved");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
