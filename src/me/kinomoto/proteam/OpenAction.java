acedf6c311995a21c9d906f0e45c64ece841ab97
Monika <monika.goszcz94@gmail.com>
2015-05-13 04:01:54 +0200

    open and save option introductory implementation 3

--- src/me/kinomoto/proteam/OpenAction.java
+++ src/me/kinomoto/proteam/OpenAction.java
@@ -1,19 +1,15 @@
 package me.kinomoto.proteam;
-import java.awt.Image;
+
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageProducer;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
-import javax.swing.Icon;
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
-
 
 public class OpenAction extends AbstractAction {
 	private static final long serialVersionUID = -2316184093044173420L;
@@ -31,12 +27,11 @@ public class OpenAction extends AbstractAction {
 
 		int ret = chooser.showOpenDialog(main);
 
-		if(ret == JFileChooser.APPROVE_OPTION)
-		{
+		if (ret == JFileChooser.APPROVE_OPTION) {
 			File file = chooser.getSelectedFile();
 			main.path = Paths.get(file.getAbsolutePath());
-			//byte[] encoded = Files.readAllBytes(main.path);
-			//main.text.setText(new String(encoded, "UTF-8"));
+			// byte[] encoded = Files.readAllBytes(main.path);
+			// main.text.setText(new String(encoded, "UTF-8"));
 			img = null;
 			try {
 				img = ImageIO.read(file);
@@ -45,11 +40,10 @@ public class OpenAction extends AbstractAction {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//ImageIO.
-			//main.surroundingsView.add
+			// ImageIO.
+			// main.surroundingsView.add
 			main.modyfied = false;
 		}
 	}
-
 
 }
--- src/me/kinomoto/proteam/SaveAsAction.java
+++ src/me/kinomoto/proteam/SaveAsAction.java
@@ -1,15 +1,8 @@
 package me.kinomoto.proteam;
+
 import java.awt.event.ActionEvent;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import javax.swing.AbstractAction;
-import javax.swing.Icon;
-import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
-
 
 public class SaveAsAction extends AbstractAction {
 	private static final long serialVersionUID = 8927972004033724123L;
@@ -23,9 +16,8 @@ public class SaveAsAction extends AbstractAction {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-			main.surroundingsView.save();
-			main.modyfied = false;
-		
+		main.surroundingsView.save();
+		main.modyfied = false;
 
 	}
 
